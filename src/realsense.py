f652538c06df0653e50e8c7b428a633bc184dad5
dat <dat@elementaryrobotics.com>
2018-11-06 10:20:39 -0800

    Remove pointcloud transformation and add writing of camera intrinsics. Need to add element whose job is to calculate and provide the static transform

--- src/realsense.py
+++ src/realsense.py
@@ -1,55 +1,25 @@
 import cv2
 import numpy as np
-import os
 import pyrealsense2 as rs
 import time
-from multiprocessing import Process
-from pyquaternion import Quaternion
 from atom import Element
 from atom.messages import Response
-from subprocess import run
 
 
-DEPTH_SHAPE = (640, 360)
+DEPTH_SHAPE = (640, 480)
 COLOR_SHAPE = (640, 480)
-TRANSFORM_FILE_PATH = "data/transform.csv"
-CALIBRATION_CLIENT_PATH = "build/transform_estimation"
-FPS = 10
+FPS = 30
 
 
-def load_transform_from_file(fname):
-    """
-    Opens specified file, reads transform, and returns as list.
-
-    Args:
-        fname (str): CSV file that stores the transform
-    """
-    with open(fname, "r") as f:
-        transform = [float(v) for v in f.readlines()[-1].split(",")]
-        return transform
-
-
-def run_calibration_client(*args):
-    """
-    Runs the transform estimation procedure, which saves the transform to disk.
-    """
-    result = run(CALIBRATION_CLIENT_PATH, capture_output=True)
-    return Response(err_code=result.returncode, err_str=result.stderr.decode())
-        
-
-def calibrate_vertices(vertices, translation, quaternion):
-    """
-    Args:
-        vertices (numpy array): vertices to be calibrated
-        translation (list): translation in the format [x, y, z]
-        quaternion (list): quaternion in the format [qx, qy, qz, qw]
-    """
-    # Pyquaternion expects [qw, qx, qy, qz]
-    q = Quaternion(quaternion[-1:]+quaternion[:-1])
-    rotation = q.rotation_matrix.astype(np.float32)
-    translation = np.array(translation, dtype=np.float32)
-    vertices = np.dot(vertices, rotation.T) + translation
-    return vertices
+def rs_intrinsics_to_dict(rs_intrinsicss):
+    return {
+        "width": rs_intrinsics.width,
+        "height": rs_intrinsics.height,
+        "ppx": rs_intrinsics.ppx,
+        "ppy": rs_intrinsics.ppy,
+        "fx": rs_intrinsics.fx,
+        "fy": rs_intrinsics.fy,
+    }
 
 
 if __name__ == "__main__":
@@ -58,8 +28,8 @@ if __name__ == "__main__":
     # Create a config and configure the pipeline to stream
     # different resolutions of color and depth streams
     config = rs.config()
-    config.enable_stream(rs.stream.depth, DEPTH_SHAPE[0], DEPTH_SHAPE[1], rs.format.z16, 15)
-    config.enable_stream(rs.stream.color, COLOR_SHAPE[0], COLOR_SHAPE[1], rs.format.bgr8, 15)
+    config.enable_stream(rs.stream.depth, DEPTH_SHAPE[0], DEPTH_SHAPE[1], rs.format.z16, FPS)
+    config.enable_stream(rs.stream.color, COLOR_SHAPE[0], COLOR_SHAPE[1], rs.format.bgr8, FPS)
 
     profile = pipeline.start(config)
 
@@ -68,34 +38,19 @@ if __name__ == "__main__":
     depth_scale = depth_sensor.get_depth_scale()
 
     # Create an align object
-    # rs.align allows us to perform alignment of depth frames to others frames
+    # rs.align allows us to perform alignment of depth frames to other frames
     align_to = rs.stream.color
     align = rs.align(align_to)
 
     rs_pc = rs.pointcloud()
-    transform = None
-    transform_last_loaded = 0
+    rs_intrinsics = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+    intrinsics = rs_intrinsics_to_dict(rs_intrinsics)
 
     element = Element("realsense")
-    element.command_add("calibrate", run_calibration_client, timeout=2000)
-
-    p = Process(target=element.command_loop)
-    p.start()
     print("Realsense started. Publishing frames.")
     try:
         while True:
             start_time = time.time()
-
-            # Load transform from file if the file exists
-            # and has been modified since we last checked
-            if os.path.exists(TRANSFORM_FILE_PATH):
-                transform_last_modified = os.stat(TRANSFORM_FILE_PATH).st_mtime
-                if transform_last_modified > transform_last_loaded:
-                    try:
-                        transform = load_transform_from_file(TRANSFORM_FILE_PATH)
-                        transform_last_loaded = time.time()
-                    except Exception as e:
-                        print(e)
 
             frames = pipeline.wait_for_frames()
             aligned_frames = align.process(frames)
@@ -124,15 +79,9 @@ if __name__ == "__main__":
             element.entry_write("color", {"data": color_serialized.tobytes()}, maxlen=FPS)
             element.entry_write("depth", {"data": depth_serialized.tobytes()}, maxlen=FPS)
             element.entry_write("pointcloud", {"data": pc_serialized.tobytes()}, maxlen=FPS)
-            if transform:
-                vertices = calibrate_vertices(vertices, transform[:3], transform[3:])
-                _, pc_serialized = cv2.imencode(".tif", vertices)
-                element.entry_write("pointcloud_calibrated", 
-                    {"data": pc_serialized.tobytes()}, maxlen=FPS)
+            element.entry_write("intrinsics", intrinsics, maxlen=FPS)
             time.sleep(max(1/FPS - (time.time() - start_time), 0))
 
     finally:
         pipeline.stop()
-        p.terminate()
-        p.join()
         del element
