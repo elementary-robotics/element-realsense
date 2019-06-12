7969c396b780b4feb43d6845dd346aa81d783e36
Peter Lipay <plipay1@gmail.com>
2019-06-12 16:06:00 -0700

    Use data contracts for all stream messages (#28)
    Use data contracts for all streams/commands, consolidate to single element
    - contracts.py defined with all API definitions codified in contract classes
    - python scripts combined to run single element that can both publish streams and handle commands
    - transform_publisher no longer a seperate process
    - all stream/command init/publishing uses the contract classes now

--- realsense/stream.py
+++ realsense/stream.py
@@ -3,25 +3,48 @@ import math
 import numpy as np
 import os
 import pyrealsense2 as rs
+import subprocess
+import threading
 import time
 from atom import Element
 from atom.messages import Response, LogLevel
+from contracts import IntrinsicsStreamContract, AccelStreamContract, GyroStreamContract, \
+ColorStreamContract, DepthStreamContract, PointCloudStreamContract, TransformStreamContract, \
+CalculateTransformCommand, REALSENSE_ELEMENT_NAME
 
+TRANSFORM_FILE_PATH = "data/transform.csv"
+CALIBRATION_CLIENT_PATH = "build/transform_estimation"
 DEPTH_SHAPE = (640, 480)
 COLOR_SHAPE = (640, 480)
 FPS = 30
 
+def load_transform_from_file(fname):
+    """
+    Opens specified file, reads transform, and returns as list.
 
-def rs_intrinsics_to_dict(rs_intrinsics):
-    return {
-        "width": rs_intrinsics.width,
-        "height": rs_intrinsics.height,
-        "ppx": rs_intrinsics.ppx,
-        "ppy": rs_intrinsics.ppy,
-        "fx": rs_intrinsics.fx,
-        "fy": rs_intrinsics.fy,
-    }
+    Args:
+        fname (str): CSV file that stores the transform
+    """
+    with open(fname, "r") as f:
+        transform_list = [float(v) for v in f.readlines()[-1].split(",")]
+        return TransformStreamContract(
+            x=transform_list[0], y=transform_list[1], z=transform_list[2],
+            qx=transform_list[3], qy=transform_list[4], qz=transform_list[5], qw=transform_list[6]
+        )
 
+
+def run_transform_estimator(*args):
+    """
+    Runs the transform estimation procedure, which saves the transform to disk.
+    """
+    process = subprocess.Popen(CALIBRATION_CLIENT_PATH, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    return Response(
+        data=CalculateTransformCommand.Response().to_data(),
+        err_code=process.returncode,
+        err_str=err.decode(),
+        serialize=CalculateTransformCommand.Response.SERIALIZE
+    )
 
 if __name__ == "__main__":
     try:
@@ -66,11 +89,29 @@ if __name__ == "__main__":
 
     rs_pc = rs.pointcloud()
     rs_intrinsics = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
-    intrinsics = rs_intrinsics_to_dict(rs_intrinsics)
+    intrinsics = IntrinsicsStreamContract(
+        width=rs_intrinsics.width,
+        height=rs_intrinsics.height,
+        ppx=rs_intrinsics.ppx,
+        ppy=rs_intrinsics.ppy,
+        fx=rs_intrinsics.fx,
+        fy=rs_intrinsics.fy
+    )
 
-    element = Element("realsense")
+    element = Element(REALSENSE_ELEMENT_NAME)
+    # Startup command thread
+    transform = TransformStreamContract(x=0, y=0, z=0, qx=0, qy=0, qz=0, qw=1)
+    transform_last_loaded = 0
+    element.command_add(
+        CalculateTransformCommand.COMMAND_NAME,
+        run_transform_estimator,
+        timeout=2000, deserialize=CalculateTransformCommand.Request.SERIALIZE
+    )
+    t = threading.Thread(target=element.command_loop, daemon=True)
+    t.start()
     element.log(LogLevel.INFO, "Realsense started. Publishing frames.")
-    element.entry_write("intrinsics", intrinsics, maxlen=FPS, serialize=True)
+
+    element.entry_write(IntrinsicsStreamContract.STREAM_NAME, intrinsics.to_dict(), serialize=IntrinsicsStreamContract.SERIALIZE, maxlen=FPS)
     try:
         while True:
             start_time = time.time()
@@ -107,14 +148,35 @@ if __name__ == "__main__":
             if is_d435i:
                 accel = frames[2].as_motion_frame().get_motion_data()
                 gyro = frames[3].as_motion_frame().get_motion_data()
-                accel_data = {"x": accel.x, "y": accel.y, "z": accel.z}
-                gyro_data = {"x": gyro.x, "y": gyro.y, "z": gyro.z}
-                element.entry_write("accel", accel_data, serialize=True, maxlen=FPS)
-                element.entry_write("gyro", gyro_data, serialize=True, maxlen=FPS)
+                accel_data = AccelStreamContract(x=accel.x, y=accel.y, z=accel.z)
+                gyro_data = GyroStreamContract(x=gyro.x, y=gyro.y, z=gyro.z)
+                element.entry_write(AccelStreamContract.STREAM_NAME, accel_data.to_dict(), serialize=AccelStreamContract.SERIALIZE, maxlen=FPS)
+                element.entry_write(GyroStreamContract.STREAM_NAME, gyro_data.to_dict(), serialize=GyroStreamContract.SERIALIZE, maxlen=FPS)
 
-            element.entry_write("color", {"data": color_serialized.tobytes()}, maxlen=FPS)
-            element.entry_write("depth", {"data": depth_serialized.tobytes()}, maxlen=FPS)
-            element.entry_write("pointcloud", {"data": pc_serialized.tobytes()}, maxlen=FPS)
+            color_contract = ColorStreamContract(data=color_serialized.tobytes())
+            depth_contract = DepthStreamContract(data=depth_serialized.tobytes())
+            pc_contract = PointCloudStreamContract(data=pc_serialized.tobytes())
+            element.entry_write(ColorStreamContract.STREAM_NAME, color_contract.to_dict(), serialize=ColorStreamContract.SERIALIZE, maxlen=FPS)
+            element.entry_write(DepthStreamContract.STREAM_NAME, depth_contract.to_dict(), serialize=DepthStreamContract.SERIALIZE, maxlen=FPS)
+            element.entry_write(PointCloudStreamContract.STREAM_NAME, pc_contract.to_dict(), serialize=PointCloudStreamContract.SERIALIZE, maxlen=FPS)
+
+            # Load transform from file if the file exists
+            # and has been modified since we last checked
+            if os.path.exists(TRANSFORM_FILE_PATH):
+                transform_last_modified = os.stat(TRANSFORM_FILE_PATH).st_mtime
+                if transform_last_modified > transform_last_loaded:
+                    try:
+                        transform = load_transform_from_file(TRANSFORM_FILE_PATH)
+                        transform_last_loaded = time.time()
+                    except Exception as e:
+                        element.log(LogLevel.ERR, str(e))
+            element.entry_write(
+                TransformStreamContract.STREAM_NAME,
+                transform.to_dict(),
+                serialize=TransformStreamContract.SERIALIZE,
+                maxlen=FPS
+            )
+
             time.sleep(max(1 / FPS - (time.time() - start_time), 0))
 
     finally:
